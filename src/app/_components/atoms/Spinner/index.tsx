eff41f0668a05ee73eb2b55a607b833b8f2c4d56
ninomiyadaisuke <daisukeninomiya0375@gmail.com>
2023-07-24 11:20:44 +0900

    fix Spinner component

--- src/app/_components/atoms/Spinner/index.tsx
+++ src/app/_components/atoms/Spinner/index.tsx
@@ -1,9 +1,16 @@
 import React, { FC } from "react";
 
-export const Spinner: FC = () => {
+type Props = {
+  variant?: "small" | "default";
+} & React.ComponentPropsWithoutRef<"span">;
+
+export const Spinner: FC<Props> = ({ variant = "default" }) => {
   return (
     <div className="flex justify-center" aria-label="読み込み中">
-      <span className="animate-spin h-9 w-9 border-4 border-red-400 rounded-full border-t-transparent" />
+      <span
+        className="animate-spin h-9 w-9 border-4 border-red-400 rounded-full border-t-transparent data-[variant=default]:h-9 data-[variant=default]:w-9 data-[variant=small]:h-6 data-[variant=small]:w-6"
+        data-variant={variant}
+      />
     </div>
   );
 };
