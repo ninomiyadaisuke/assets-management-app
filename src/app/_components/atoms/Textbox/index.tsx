e6c340f9718b3a108f59b7aadcef93167ab6f666
ninomiyadaisuke <daisukeninomiya0375@gmail.com>
2023-07-26 14:43:05 +0900

    fix Textbox

--- src/app/_components/atoms/Textbox/index.tsx
+++ src/app/_components/atoms/Textbox/index.tsx
@@ -1,15 +1,17 @@
 import { forwardRef } from "react";
 
-type Props = Omit<React.ComponentPropsWithRef<"input">, "type" | "className">;
+type Props = Omit<React.ComponentPropsWithRef<"input">, "className"> & {
+  type?: "text" | "password" | "tel";
+};
 
 export const Textbox = forwardRef<HTMLInputElement, Props>(function TextboxBase(
-  props,
+  { type = "text", ...props },
   ref
 ) {
   return (
     <input
       ref={ref}
-      type="text"
+      type={type}
       {...props}
       className="inline-block w-full rounded-md border border-gray-300 bg-gray-200 px-2.5 py-1.5 align-middle text-base text-gray-600 transition duration-200 focus:bg-white focus:outline-none
       disabled:opacity-50  aria-invalid:border-red-500 aria-invalid:bg-input-error"
