ec996b27b7fb68218668f9bbcc401c583e7cff32
ninomiyadaisuke <daisukeninomiya0375@gmail.com>
2023-07-28 11:21:07 +0900

    delete code

--- src/app/(assets)/page.tsx
+++ src/app/(assets)/page.tsx
@@ -1,13 +1,8 @@
-import { JaStockAccordion } from "../_components/templates/JaStocks/JaStockAccordion";
-
 export default function Home() {
   return (
     <>
       <div>Toppage</div>
       <h1>Hello world</h1>
-      <div className="m-auto w-[80%]">
-        <JaStockAccordion />
-      </div>
     </>
   );
 }
