005f47fc64f79fcd24da9a8267f9dbf874a91815
ninomiyadaisuke <daisukeninomiya0375@gmail.com>
2023-07-30 13:36:58 +0900

    create SearchedInfoTable component

---
+++ src/app/_components/atoms/SearchedInfo/index.tsx
@@ -0,0 +1,19 @@
+import { FC } from "react";
+
+type Props = {
+  value: string | number;
+  unit?: "円" | "＄" | "%";
+  title: string;
+};
+
+export const SearchedInfo: FC<Props> = ({ value, unit, title }) => {
+  return (
+    <tr className="flex border-b border-gray-200 p-3">
+      <th className="flex-1 text-left font-light">{title}</th>
+      <td className="flex-1 text-right">
+        {value.toLocaleString()}
+        {unit}
+      </td>
+    </tr>
+  );
+};
---
+++ src/app/_components/organisms/SearchedInfoTable/index.tsx
@@ -0,0 +1,34 @@
+"use client";
+import { FC } from "react";
+
+import { SearchedInfo } from "../../atoms/SearchedInfo";
+
+type Props = {
+  unit: "＄" | "円";
+};
+
+const data = ["株価", "配当金", "配当利回り"];
+
+export const SearchedInfoTable: FC<Props> = ({ unit }) => {
+  const stockName = "日本電信電話";
+  const latestStockPrice = 2000;
+  const dividend = 4;
+  const dividendYield = 4.1;
+  const array = [latestStockPrice, dividend, dividendYield];
+  return (
+    <table className="w-full border px-3">
+      <thead className="block border-b border-gray-200 bg-blue-50 py-3 text-center font-bold">
+        {stockName}
+      </thead>
+      <tbody className="w-full">
+        {data.map((data, i) => (
+          <SearchedInfo
+            title={data}
+            value={array[i]}
+            unit={data !== "配当利回り" ? unit : "%"}
+          />
+        ))}
+      </tbody>
+    </table>
+  );
+};
