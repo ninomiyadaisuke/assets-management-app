25df76cae5685c2afe6ceface2bda3f5caf57c97
ninomiyadaisuke <daisukeninomiya0375@gmail.com>
2023-07-31 13:39:59 +0900

    create JaStockCreate Modal page

--- src/app/(assets)/@modal/(.)create/ja/page.tsx
+++ src/app/(assets)/@modal/(.)create/ja/page.tsx
@@ -1,6 +1,6 @@
 import DialogWrapper from "@/app/_components/molecules/DaialogWrapper";
 import { JaStockCreate } from "@/app/_components/templates/JaStockCreate";
-export default function Home() {
+export default function JaStokCreateModalPage() {
   return (
     <DialogWrapper>
       <JaStockCreate />
