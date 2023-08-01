eefb72847e7f7756882ccd29d5e14c9929bdf7ea
ninomiyadaisuke <daisukeninomiya0375@gmail.com>
2023-08-02 08:31:01 +0900

    display AlertDaialog

--- src/app/_components/templates/JaStockCreate/index.tsx
+++ src/app/_components/templates/JaStockCreate/index.tsx
@@ -1,16 +1,31 @@
+"use client";
 import { FC } from "react";
 
 import { RadioGroup } from "@/app/_components/molecules/RadioGroup";
-
-import { JaStockCreateForm } from "./JaStockCreateForm";
-import { SearchForm } from "./SearchForm";
+import { JaStockCreateForm } from "@/app/_components/templates/JaStockCreate/JaStockCreateForm";
+import { SearchForm } from "@/app/_components/templates/JaStockCreate/SearchForm";
+import { useAlertDialog } from "@/hooks/useAlertDialog";
+import { useStockStatus } from "@/hooks/useStockStatus";
 
 export const JaStockCreate: FC = () => {
+  const { stockName } = useStockStatus();
+  const { showAlertDialog, hideAlertDialog } = useAlertDialog();
   return (
     <div className="flex grow flex-col gap-9">
       <SearchForm />
       <RadioGroup />
-      <JaStockCreateForm />
+      <JaStockCreateForm
+        title="新規株式登録"
+        onClickSave={() =>
+          showAlertDialog({ message: `新しく${stockName}を登録しますか？` })
+        }
+        onValid={async (input) => {
+          hideAlertDialog();
+          try {
+          } catch (error) {}
+        }}
+        onInvalid={() => hideAlertDialog()}
+      />
     </div>
   );
 };
