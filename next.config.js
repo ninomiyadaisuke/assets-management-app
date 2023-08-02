76a02364a697277d878ac343ddb244203349bda3
ninomiyadaisuke <daisukeninomiya0375@gmail.com>
2023-08-02 15:37:25 +0900

    fix next config

--- next.config.js
+++ next.config.js
@@ -1,12 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    outputFileTracingExcludes: {
+      "*": [
+        "node_modules/@swc/core-linux-x64-gnu",
+        "node_modules/@swc/core-linux-x64-musl",
+        "node_modules/@esbuild/linux-x64",
+      ],
+    },
+  },
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
-      use: ['@svgr/webpack'],
+      use: ["@svgr/webpack"],
     });
     return config;
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
