6cc8cb4c090285d5e585e133c5286992151647d5
ninomiyadaisuke <daisukeninomiya0375@gmail.com>
2023-08-04 08:15:37 +0900

    test deploy

--- src/app/_components/templates/JaStocks/JaStocksPriceCard/index.tsx
+++ src/app/_components/templates/JaStocks/JaStocksPriceCard/index.tsx
@@ -3,16 +3,19 @@ import { FC } from "react";
 import { CardWrapper } from "@/app/_components/atoms/CardWrapper";
 import { TotalProfitAndLoss } from "@/app/_components/atoms/TotalProfitAndLoss";
 import { TotalStocks } from "@/app/_components/atoms/TotalStocks";
-import { getJaStocksTotal } from "@/services/server/jaStockTotal";
+import { jaStockTotal } from "@/services/server/jaStockTotal";
 
 export const JaStocksPriceCard: FC = async () => {
   const { currentStockPriceSum, profitLossAmount, evaluationProfitLossRate } =
-    await getJaStocksTotal();
+    await jaStockTotal();
 
   return (
     <CardWrapper>
-      <TotalStocks total={0} />
-      <TotalProfitAndLoss profitMargin={0} profitAndLossAmount={0} />
+      <TotalStocks total={currentStockPriceSum} />
+      <TotalProfitAndLoss
+        profitMargin={evaluationProfitLossRate}
+        profitAndLossAmount={profitLossAmount}
+      />
     </CardWrapper>
   );
 };
