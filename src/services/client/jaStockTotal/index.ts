573e0738063544672f1afdf541976dc2ca079142
ninomiyadaisuke <daisukeninomiya0375@gmail.com>
2023-08-04 08:37:25 +0900

    test deploy

--- src/services/client/jaStockTotal/index.ts
+++ src/services/client/jaStockTotal/index.ts
@@ -1,16 +1,16 @@
-// import { headers } from "next/headers";
+import { headers } from "next/headers";
 
-// import { typedFetch } from "@/libs/fetchUtils";
-// import type { JaStockTotalReturn } from "@/services/server/jaStockTotal";
+import { typedFetch } from "@/libs/fetchUtils";
+import type { JaStockTotalReturn } from "@/services/server/jaStockTotal";
 
-// export const getJaStocksTotal = async () => {
-//   return typedFetch<JaStockTotalReturn>(
-//     `${process.env.NEXT_PUBLIC_API_URL}/api/stocks/ja/total`,
-//     {
-//       headers: {
-//         cookie: headers().get("cookie") as string,
-//       },
-//       cache: "no-store",
-//     }
-//   );
-// };
+export const getJaStocksTotal = async () => {
+  return typedFetch<JaStockTotalReturn>(
+    `${process.env.NEXT_PUBLIC_API_URL}/api/stocks/ja/total`,
+    {
+      headers: {
+        cookie: headers().get("cookie") as string,
+      },
+      cache: "no-store",
+    }
+  );
+};
