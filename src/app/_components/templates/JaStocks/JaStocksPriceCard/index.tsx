410f722c80289b52fa8e05b68b084fc44c92b730
ninomiyadaisuke <daisukeninomiya0375@gmail.com>
2023-08-03 20:10:24 +0900

    test deploy

--- src/app/_components/templates/JaStocks/JaStocksPriceCard/index.tsx
+++ src/app/_components/templates/JaStocks/JaStocksPriceCard/index.tsx
@@ -5,17 +5,24 @@ import { TotalProfitAndLoss } from "@/app/_components/atoms/TotalProfitAndLoss";
 import { TotalStocks } from "@/app/_components/atoms/TotalStocks";
 import { getJaStocksTotal } from "@/services/client/jaStockTotal";
 
+const test = async () => {
+  const res = await fetch("http://localhost:3000/api/stocks/ja/total", {
+    method: "GET",
+  });
+  const data = await res.json();
+  return data;
+};
+
 export const JaStocksPriceCard: FC = async () => {
-  const { currentStockPriceSum, profitLossAmount, evaluationProfitLossRate } =
-    await getJaStocksTotal();
+  // const { currentStockPriceSum, profitLossAmount, evaluationProfitLossRate } =
+  //   await getJaStocksTotal();
+  const data = await test();
+  // console.log(data);
 
   return (
     <CardWrapper>
-      <TotalStocks total={currentStockPriceSum} />
-      <TotalProfitAndLoss
-        profitMargin={evaluationProfitLossRate}
-        profitAndLossAmount={profitLossAmount}
-      />
+      <TotalStocks total={0} />
+      <TotalProfitAndLoss profitMargin={10} profitAndLossAmount={10} />
     </CardWrapper>
   );
 };
