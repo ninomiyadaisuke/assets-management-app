fe27686da1da5e488c714210512e46ecc8eee5da
ninomiyadaisuke <daisukeninomiya0375@gmail.com>
2023-08-04 11:34:41 +0900

    test deploy

--- src/app/(assets)/api/stocks/ja/total/route.ts
+++ src/app/(assets)/api/stocks/ja/total/route.ts
@@ -7,25 +7,19 @@ import { Database } from "@/libs/database.types";
 import { UnauthorizedError } from "@/libs/error";
 import { jaStockTotal } from "@/services/server/jaStockTotal";
 
-const createRouteHandlerClientCache = cache(() => {
-  const cookieStore = cookies();
-  return createRouteHandlerClient<Database>({ cookies: () => cookieStore });
-});
-
 export async function GET() {
-  try {
-    const supabase = createRouteHandlerClientCache();
-    const {
-      data: { user },
-    } = await supabase.auth.getUser();
-    if (!user) throw new UnauthorizedError();
-    const uid = user.id;
-    const data = await jaStockTotal(uid);
-    return NextResponse.json(data);
-    // return uid;
-  } catch (error) {
-    if (error instanceof UnauthorizedError) {
-      throw error;
-    }
-  }
+  const createRouteHandlerClientCache = cache(() => {
+    const cookieStore = cookies();
+    return createRouteHandlerClient<Database>({ cookies: () => cookieStore });
+  });
+  const supabase = createRouteHandlerClientCache();
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+  if (!user) throw new UnauthorizedError();
+  const uid = user.id;
+  // return uid;
+  const data = await jaStockTotal(uid);
+
+  return NextResponse.json(data);
 }
