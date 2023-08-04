a6e3f8565c3c537711ff25589cab0254cd790f15
ninomiyadaisuke <daisukeninomiya0375@gmail.com>
2023-08-04 12:36:37 +0900

    test deploy

--- src/services/server/auth.ts
+++ src/services/server/auth.ts
@@ -1,18 +1,18 @@
-import { createRouteHandlerClient } from "@supabase/auth-helpers-nextjs";
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
 import { cookies } from "next/headers";
 import { cache } from "react";
 
 import { Database } from "@/libs/database.types";
 import { InternalServerError, UnauthorizedError } from "@/libs/error";
 
-const createRouteHandlerClientCache = cache(() => {
+const createServerComponentClientCache = cache(() => {
   const cookieStore = cookies();
-  return createRouteHandlerClient<Database>({ cookies: () => cookieStore });
+  return createServerComponentClient<Database>({ cookies: () => cookieStore });
 });
 
 export const authValidateAndReturnUid = async (): Promise<string> => {
   try {
-    const supabase = createRouteHandlerClientCache();
+    const supabase = createServerComponentClientCache();
     const {
       data: { user },
     } = await supabase.auth.getUser();
