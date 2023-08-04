6452fdcc71107ab119b7bdbb4e2b7e9b616698a0
ninomiyadaisuke <daisukeninomiya0375@gmail.com>
2023-08-04 09:08:18 +0900

    test deploy

--- src/app/api/stocks/ja/total/route.ts
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from "next/server";
-
-import { jaStockTotal } from "@/services/server/jaStockTotal";
-
-export async function GET() {
-  const data = await jaStockTotal();
-
-  return NextResponse.json(data);
-}
