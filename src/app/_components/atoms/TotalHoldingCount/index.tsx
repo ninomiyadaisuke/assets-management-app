d9959a8550390267fd15ecb630cb28f570021de9
ninomiyadaisuke <daisukeninomiya0375@gmail.com>
2023-08-10 14:56:33 +0900

    fix TotalHoldingCount

--- src/app/_components/atoms/TotalHoldingCount/index.tsx
+++ src/app/_components/atoms/TotalHoldingCount/index.tsx
@@ -14,14 +14,25 @@ const createServerComponentClientCache = cache(() => {
   const cookieStore = cookies();
   return createServerComponentClient<Database>({ cookies: () => cookieStore });
 });
+const serverComponentAuthValidateAndReturnUid = async (): Promise<string> => {
+  try {
+    const supabase = createServerComponentClientCache();
+    const {
+      data: { user },
+    } = await supabase.auth.getUser();
+    if (!user) throw new UnauthorizedError();
+    const uid = user.id;
+    return uid;
+  } catch (error) {
+    if (error instanceof UnauthorizedError) {
+      throw error;
+    }
+  }
+  throw new InternalServerError();
+};
 
 export const TotalHoldingCount: FC<Props> = async ({ country }) => {
-  const supabase = createServerComponentClientCache();
-  const {
-    data: { user },
-  } = await supabase.auth.getUser();
-  if (!user) throw new UnauthorizedError();
-  const uid = user.id;
+  const uid = await serverComponentAuthValidateAndReturnUid();
   const totalCount = await fetchJaTotalHoldingCountClient(uid, country);
   return (
     <span className="text-sm font-semibold text-gray-500">{`全${totalCount}件`}</span>
