92b97bbe2d796ad8707755d39ffa17b022fcd141
ninomiyadaisuke <daisukeninomiya0375@gmail.com>
2023-08-10 16:05:00 +0900

    fix ExchangeRateWithSwitch

--- src/services/client/exchangeRate/index.ts
+++ src/services/client/exchangeRate/index.ts
@@ -10,8 +10,8 @@ export type ExchangeRateApiResponse = {
 export const fetchLatestUsdToJpyRateClient =
   async (): Promise<ExchangeRateApiResponse> => {
     return await typedFetch<ExchangeRateApiResponse>(
-      `https://v6.exchangerate-api.com/v6/${process.env.EXCHANGE_RATE_API}/latest/USD`,
-      // "http://localhost:3000/api/exchange-rate",
+      // `https://v6.exchangerate-api.com/v6/${process.env.EXCHANGE_RATE_API}/latest/USD`,
+      "http://localhost:3000/api/exchange-rate",
       {
         next: { revalidate: 30000 },
       }
