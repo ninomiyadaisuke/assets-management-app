211906338888063750d4d9558ebfa7074eb76190
ninomiyadaisuke <daisukeninomiya0375@gmail.com>
2023-08-10 14:34:45 +0900

    fix TotalHoldingCount

--- src/app/_components/atoms/TotalHoldingCount/index.tsx
+++ src/app/_components/atoms/TotalHoldingCount/index.tsx
@@ -1,15 +1,40 @@
-import { FC } from "react";
+import {
+  createRouteHandlerClient,
+  createServerComponentClient,
+} from "@supabase/auth-helpers-nextjs";
+import { cookies } from "next/headers";
+import { cache, FC } from "react";
 
+import { Database } from "@/libs/database.types";
+import { InternalServerError, UnauthorizedError } from "@/libs/error";
 import { fetchJaTotalHoldingCountClient } from "@/services/client/totalHoldingCount";
 
 type Props = {
   country: "ja" | "foreign";
 };
 
+const createServerComponentClientCache = cache(() => {
+  const cookieStore = cookies();
+  return createServerComponentClient<Database>({ cookies: () => cookieStore });
+});
+const serverComponentAuthValidateAndReturnUid = async (): Promise<string> => {
+  try {
+    const supabase = createServerComponentClientCache();
+    const {
+      data: { user },
+    } = await supabase.auth.getUser();
+    if (!user) throw new UnauthorizedError();
+    const uid = user.id;
+    return uid;
+  } catch (error) {
+    if (error instanceof UnauthorizedError) {
+      throw error;
+    }
+  }
+  throw new InternalServerError();
+};
+
 export const TotalHoldingCount: FC<Props> = async ({ country }) => {
-  const { serverComponentAuthValidateAndReturnUid } = await import(
-    "@/services/server/test"
-  );
   const uid = await serverComponentAuthValidateAndReturnUid();
   const totalCount = await fetchJaTotalHoldingCountClient(uid, country);
   return (
