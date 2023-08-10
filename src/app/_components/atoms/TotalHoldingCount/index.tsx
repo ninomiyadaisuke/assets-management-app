91e4adb0a282acd39031fdabfcfadc7c11ebb48f
ninomiyadaisuke <daisukeninomiya0375@gmail.com>
2023-08-10 14:18:25 +0900

    fix TotalHoldingCount

--- src/app/_components/atoms/TotalHoldingCount/index.tsx
+++ src/app/_components/atoms/TotalHoldingCount/index.tsx
@@ -1,13 +1,15 @@
 import { FC } from "react";
 
 import { fetchJaTotalHoldingCountClient } from "@/services/client/totalHoldingCount";
-import { serverComponentAuthValidateAndReturnUid } from "@/services/server/test";
 
 type Props = {
   country: "ja" | "foreign";
 };
 
 export const TotalHoldingCount: FC<Props> = async ({ country }) => {
+  const { serverComponentAuthValidateAndReturnUid } = await import(
+    "@/services/server/test"
+  );
   const uid = await serverComponentAuthValidateAndReturnUid();
   const totalCount = await fetchJaTotalHoldingCountClient(uid, country);
   return (
