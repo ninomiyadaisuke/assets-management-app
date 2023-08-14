30442ed519f922c26979eba70de688795d48a1c9
ninomiyadaisuke <daisukeninomiya0375@gmail.com>
2023-08-14 09:14:00 +0900

    Get Selected Data

--- src/app/_components/templates/FgnStockCreate/SelectForm/index.tsx
+++ src/app/_components/templates/FgnStockCreate/SelectForm/index.tsx
@@ -2,13 +2,16 @@
 
 import { zodResolver } from "@hookform/resolvers/zod";
 import cx from "classnames";
-import { FC } from "react";
+import { FC, Fragment } from "react";
 import { SubmitHandler, useForm } from "react-hook-form";
 
 import { SearchButton } from "@/app/_components/atoms/SearchButton";
 import { ComboboxWithError } from "@/app/_components/molecules/ComboboxWithError";
+import { useLoading } from "@/hooks/useLoading";
+import { useStockStatus } from "@/hooks/useStockStatus";
 import type { SearchInputSchema } from "@/libs/schema/searchStock";
 import { searchInputSchema } from "@/libs/schema/searchStock";
+import { searchFgnStockClient } from "@/services/client/serchStock";
 
 const options = ["VYM", "HDV", "SPYD", "AGG", "ARCC"];
 
@@ -22,13 +25,37 @@ export const SelectForm: FC<Props> = (props) => {
   const {
     register,
     handleSubmit,
+    setError,
+    clearErrors,
+    reset,
     formState: { errors, isSubmitting },
   } = useForm<SearchInputSchema>({
     resolver: zodResolver(searchInputSchema),
   });
 
-  const onSubmit: SubmitHandler<SearchInputSchema> = (code) => {
-    const test = code;
+  const { setIsLoading } = useLoading();
+  const { setStockStatus } = useStockStatus();
+  const onSubmit: SubmitHandler<SearchInputSchema> = async (code) => {
+    setIsLoading(true);
+    try {
+      const stockData = await searchFgnStockClient(code);
+      if ("message" in stockData) {
+        setError("code", {
+          type: "manual",
+          message: "この株式はすでに保有しています。",
+        });
+        setIsLoading(false);
+        return;
+      }
+      clearErrors("code");
+      if ("stockCode" in stockData) setStockStatus(stockData);
+      setIsLoading(false);
+      reset();
+    } catch (error) {
+      throw new Error("失敗しました。");
+    } finally {
+      setIsLoading(false);
+    }
   };
 
   return (
@@ -43,12 +70,10 @@ export const SelectForm: FC<Props> = (props) => {
         disabled={isSubmitting}
       >
         {options.map((label) => (
-          <>
+          <Fragment key={label}>
             <option hidden>選択してください</option>
-            <option key={label} value={label}>
-              {label}
-            </option>
-          </>
+            <option value={label}>{label}</option>
+          </Fragment>
         ))}
       </ComboboxWithError>
       <SearchButton disabled={isSubmitting} />
