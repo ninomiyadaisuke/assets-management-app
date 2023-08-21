5c8bd60d89030504426093bdba893f2f0e6d8867
ninomiyadaisuke <daisukeninomiya0375@gmail.com>
2023-08-21 15:07:28 +0900

    create DouhnutChart component

--- src/app/_components/organisms/DoughnutChart/index.tsx
+++ src/app/_components/organisms/DoughnutChart/index.tsx
@@ -1,5 +1,40 @@
+"use client";
+
+import { ResponsivePie } from "@nivo/pie";
 import { FC } from "react";
 
-export const DoughnutChart: FC = () => {
-  return <div>DoughnutChart</div>;
+import { extractColorsUpToIndex } from "@/libs/utils";
+
+type Props = {
+  total: number;
+  title: string;
+  data: {
+    id: string;
+    value: number;
+    label?: string;
+  }[];
+};
+export const DoughnutChart: FC<Props> = ({ data, total, title }) => {
+  return (
+    <div className="relative h-[300px]">
+      <ResponsivePie
+        data={data}
+        margin={{ top: 40, right: 80, bottom: 80, left: 80 }}
+        innerRadius={0.5}
+        colors={extractColorsUpToIndex(data.length)}
+        valueFormat={(value) => `${value}%`}
+        padAngle={0.5}
+        activeOuterRadiusOffset={8}
+        arcLinkLabelsSkipAngle={10}
+        arcLinkLabelsTextColor="#333333"
+        arcLinkLabelsThickness={1}
+        arcLabelsSkipAngle={20}
+        arcLabelsTextColor="#fff"
+      />
+      <div className="absolute left-2/4 top-[108px] flex translate-x-[-50%] flex-col text-center text-sm">
+        <p>{title}</p>
+        <p className="text-xs">{total.toLocaleString()}円</p>
+      </div>
+    </div>
+  );
 };
