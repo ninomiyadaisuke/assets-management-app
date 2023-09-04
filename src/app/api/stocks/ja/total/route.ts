23bbd601a43fcb6c48ad3c70b937812112456b6b
ninomiyadaisuke <daisukeninomiya0375@gmail.com>
2023-09-04 13:12:40 +0900

    fix jastocks total

--- src/app/api/stocks/ja/total/route.ts
+++ src/app/api/stocks/ja/total/route.ts
@@ -4,13 +4,12 @@ import { UnauthorizedError } from "@/libs/error";
 import { createRouteHandlerClientCache } from "@/services/server";
 import { jaStockTotal } from "@/services/server/jaStockTotal";
 
-export async function GET() {
-  const supabase = createRouteHandlerClientCache();
-  const {
-    data: { user },
-  } = await supabase.auth.getUser();
-  if (!user) throw new UnauthorizedError();
-  const uid = user.id;
+export async function GET(
+  request: Request,
+  { params }: { params: { slug: string } }
+) {
+  const { searchParams } = new URL(request.url);
+  const uid = searchParams.get("q");
 
   const data = await jaStockTotal(uid);
 
--- src/services/client/jaStockTotal/index.ts
+++ src/services/client/jaStockTotal/index.ts
@@ -4,11 +4,11 @@ import { typedFetch } from "@/libs/fetchUtils";
 import { url } from "@/services/client/url";
 import type { JaStockTotalReturn } from "@/services/server/jaStockTotal";
 
-export const fetchJaStocksTotal = async () => {
-  return typedFetch<JaStockTotalReturn>(`${url}/api/stocks/ja/total`, {
-    headers: {
-      cookie: headers().get("cookie") as string,
-    },
-    cache: "no-store",
-  });
+export const fetchJaStocksTotal = async (uid: string) => {
+  const params = { q: uid };
+  const query = new URLSearchParams(params);
+  return typedFetch<JaStockTotalReturn>(
+    `${url}/api/stocks/ja/total?${query}`,
+    {}
+  );
 };
