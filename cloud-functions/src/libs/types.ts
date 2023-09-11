75cced3d5e7b5a78cb29011219777af6e57156fa
ninomiyadaisuke <daisukeninomiya0375@gmail.com>
2023-09-11 11:23:30 +0900

    buld update of dividends

--- cloud-functions/src/libs/types.ts
+++ cloud-functions/src/libs/types.ts
@@ -1,10 +1,12 @@
 export type Result = {
   stockId: string;
   currentStockPrice: number;
+  dividend: number;
 };
 
 export type Stock = {
   stockId: string;
   stockName: string;
   currentStockPrice: number;
+  dividend: number;
 };
