c097f0df052da15fcb75bf5023a77736766602a6
ninomiyadaisuke <daisukeninomiya0375@gmail.com>
2023-09-19 15:17:05 +0900

    get user message

--- src/app/_components/layouts/Header/Nav/index.tsx
+++ src/app/_components/layouts/Header/Nav/index.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { Prisma } from "@prisma/client";
 import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
 import { useRouter } from "next/navigation";
 import { FC } from "react";
@@ -6,9 +7,25 @@ import { FC } from "react";
 import { IconButton } from "@/app/_components/atoms/IconButton";
 import type { Database } from "@/libs/database.types";
 
-export const Nav: FC = () => {
+type Props = {
+  userMessagePromise: Prisma.PrismaPromise<
+    {
+      message: {
+        content: string;
+        messageId: string;
+      };
+      isRead: boolean;
+      readAt: Date | null;
+      messageId: string;
+      userMessageId: string;
+    }[]
+  >;
+};
+
+export const Nav: FC<Props> = ({ userMessagePromise }) => {
   const router = useRouter();
   const supabase = createClientComponentClient<Database>();
+
   const handleSignOut = async () => {
     await supabase.auth.signOut();
     router.refresh();
--- src/app/_components/layouts/Header/index.tsx
+++ src/app/_components/layouts/Header/index.tsx
@@ -1,13 +1,29 @@
+import { Prisma } from "@prisma/client";
 import { FC } from "react";
 
 import { Heading } from "./Heading";
 import { Nav } from "./Nav";
 
-export const Header: FC = () => {
+type Props = {
+  userMessagePromise: Prisma.PrismaPromise<
+    {
+      message: {
+        content: string;
+        messageId: string;
+      };
+      isRead: boolean;
+      readAt: Date | null;
+      messageId: string;
+      userMessageId: string;
+    }[]
+  >;
+};
+
+export const Header: FC<Props> = ({ userMessagePromise }) => {
   return (
     <header className="fixed z-50 flex h-[50px] w-full items-center justify-between bg-primary px-4 md:w-[768px]">
       <Heading />
-      <Nav />
+      <Nav userMessagePromise={userMessagePromise} />
     </header>
   );
 };
