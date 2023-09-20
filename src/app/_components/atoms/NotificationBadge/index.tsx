6335212d007e79af591a0e8da5a18019948c7778
ninomiyadaisuke <daisukeninomiya0375@gmail.com>
2023-09-20 10:01:25 +0900

    display NotificationBadge

--- src/app/_components/atoms/NotificationBadge/index.tsx
+++ src/app/_components/atoms/NotificationBadge/index.tsx
@@ -1,4 +1,6 @@
-import { BellIcon } from "@heroicons/react/20/solid";
+"use client";
+
+import { BellIcon } from "@heroicons/react/24/outline";
 import { forwardRef } from "react";
 
 type Props = React.ComponentPropsWithRef<"button"> & {
@@ -9,7 +11,7 @@ export const NotificationBadge = forwardRef<HTMLButtonElement, Props>(
   function NotificationBadgeBase({ className, count, ...props }, ref) {
     return (
       <button className="relative" ref={ref} {...props}>
-        <BellIcon className="h-6" />
+        <BellIcon className="h-7 text-white" />
         {count && (
           <span className="absolute right-[-6px] top-[-5px] flex  items-center justify-center rounded-full bg-red-600 px-1 py-[2px] text-[10px] text-white">
             {count}
